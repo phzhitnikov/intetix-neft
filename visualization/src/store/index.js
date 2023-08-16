e00c8a53c96c37ba58acfaa84a441876ed14c8c4
phzhik <phzhitnikov@gmail.com>
2023-08-16 15:50:22 +0400

    * Adjusted flask positions

--- visualization/src/store/index.js
+++ visualization/src/store/index.js
@@ -11,26 +11,26 @@ const devices = [
     {'id': '0'},  // button device
 
     // Timano
-    {'id': '12', 'address': '71 CB F8 26', 'position': {x: "2556px", y: "713px"}},
-    {'id': '13', 'address': '30 31 E1 1A', 'position': {x: "2475px", y: "470px"}},
-    {'id': '14', 'address': '51 6E 73 26', 'position': {x: "2010px", y: "538px"}},
-    {'id': '15', 'address': '95 B5 A8 AC', 'position': {x: "1456px", y: "491px"}},
+    {'id': '12', 'address': '71 CB F8 26', 'position': {x: "2556px", y: "723px"}},
+    {'id': '13', 'address': '30 31 E1 1A', 'position': {x: "2495px", y: "450px"}},
+    {'id': '14', 'address': '51 6E 73 26', 'position': {x: "1990px", y: "538px"}},
+    {'id': '15', 'address': '95 B5 A8 AC', 'position': {x: "1480px", y: "491px"}},
     {'id': '16', 'address': '12 20 12 1A', 'position': {x: "1135px", y: "921px"}},
 
     // Sibir
-    {'id': '7',  'address': '50 5B 85 1A', 'position': {x: "2568px", y: "325px"}},
+    {'id': '7',  'address': '50 5B 85 1A', 'position': {x: "2598px", y: "315px"}},
     {'id': '8',  'address': '25 4C 0A AD', 'position': {x: "2700px", y: "741px"}},
-    {'id': '9',  'address': 'C0 09 64 21', 'position': {x: "1505px", y: "1353px"}},
+    {'id': '9',  'address': 'C0 09 64 21', 'position': {x: "1505px", y: "1333px"}},
     {'id': '10', 'address': '81 5F 9D 26', 'position': {x: "1291px", y: "1724px"}},
-    {'id': '11', 'address': '81 73 94 26', 'position': {x: "1086px",  y: "678px"}},
+    {'id': '11', 'address': '81 73 94 26', 'position': {x: "1116px",  y: "678px"}},
 
     // Baku
-    {'id': '1', 'address': '71 A4 12 26', 'position': {x: "2450px", y: "1940px"}},
-    {'id': '2', 'address': '81 04 93 26', 'position': {x: "2780px", y: "1747px"}},
-    {'id': '3', 'address': '71 79 98 26', 'position': {x: "2460px", y: "1470px"}},
-    {'id': '4', 'address': '51 46 3D 26', 'position': {x: "2057px", y: "1366px"}},
-    {'id': '5', 'address': '71 92 A7 26', 'position': {x: "1520px", y: "762px"}},
-    {'id': '6', 'address': '40 6F 58 1A', 'position': {x: "1259px", y: "348px"}},
+    {'id': '1', 'address': '71 A4 12 26', 'position': {x: "2435px", y: "1920px"}},
+    {'id': '2', 'address': '81 04 93 26', 'position': {x: "2760px", y: "1727px"}},
+    {'id': '3', 'address': '71 79 98 26', 'position': {x: "2440px", y: "1450px"}},
+    {'id': '4', 'address': '51 46 3D 26', 'position': {x: "2057px", y: "1356px"}},
+    {'id': '5', 'address': '71 92 A7 26', 'position': {x: "1545px", y: "762px"}},
+    {'id': '6', 'address': '40 6F 58 1A', 'position': {x: "1259px", y: "330px"}}
 ]
 
 function getDeviceById(id) {
